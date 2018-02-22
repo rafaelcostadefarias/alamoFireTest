eefcb8678ce1160a9e6cbc00273e4792ad330232
Rafael Farias <rafaelcostadefarias@gmail.com>
2018-02-21 22:02:10 -0300

    Initial Commit

---
+++ alamoFireTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alamoFireTest
+//
+//  Created by Rafael Farias on 21/02/18.
+//  Copyright © 2018 Rafael Farias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
